4247c341a9508fbe6979a17e6b17f0ec72dac556
1hamidat <sadefisayo5@gmail.com>
2022-12-09 12:42:24 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(0)
+{
+	char a = 'z';
+
+	while (a >= 'a')
+	{
+		putchar(a);
+		a--;
+	}
+	putchar('\n');
+	return (0);
+}
